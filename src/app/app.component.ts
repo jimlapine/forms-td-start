0fa0cd6114e92b61cbb606a664eb4880f4492f95
jimlapine <jim.lapine@gmail.com>
2017-08-11 22:18:10 -0400

    Lesson updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signupForm: NgForm;
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  // We can pass the form on submit or use ViewChild
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  // We can also access the same form as a ViewChild, which gives us access to the form before submit
+  onSubmit(){
+    console.log(this.signupForm);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+// Needed for form support, this allow us to use the from template driven approach.
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
